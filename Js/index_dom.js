77906027ded4846657eb5ef6d4497edbf9ced5d2
Daniel Ayala <dayalaramz@outlook.com>
2022-03-28 13:31:01 -0500

    first commit exercises javascript Jhon Mircha

---
+++ Js/index_dom.js
@@ -0,0 +1,10 @@
+import hamburgerMenu from "./menu_hamburger.js";
+
+const d=document;
+d.addEventListener("DOMContentLoaded", e=>{           //Primero se carga el documento
+  hamburgerMenu("#panel-btn","#panel","#a");   //despues ahora si le paso los parametros
+});
+
+
+
+
---
+++ Js/menu_hamburger.js
@@ -0,0 +1,20 @@
+
+export default function hamburgerMenu(panelBtn, panel, linkSecction){
+  const d = document;
+  
+  d.addEventListener("click", e =>{   
+    console.log(e.target);    //solo me muestra a que le di click
+    
+    //click lo asigno a document
+    if(e.target.matches(panelBtn) || e.target.matches(`${panelBtn} *`) ){                           //si lo clickeado coincide con el parametro que me trajiste en panelBtn o en cualquier elemento dentro del 'panelBtn'
+      d.querySelector(panel).classList.toggle("is-active");      //busca en el selector el id que me trajiste dentro del parametro panel
+      d.querySelector(panelBtn).classList.toggle("is-active");    //busca en el selector el id que me trajiste dentro del parametro panel y lo activa o desactiva
+                                                                 // y entra a su lista de clases y con el metodo toggle le vas a intercambiar la clase 'is-active'
+      
+    } 
+    if(e.target.matches(linkSecction)){
+      d.querySelector(panel).classList.remove("is-active");
+      d.querySelector(panelBtn).classList.remove("is-active");
+    }
+  })
+}
