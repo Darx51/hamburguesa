ea587576104117c8d8d5585bdc9d89f4e4ccd626
Daniel Ayala <dayalaramz@outlook.com>
2022-03-28 19:23:50 -0500

    boton panel fixed

--- Js/index_dom.js
+++ Js/index_dom.js
@@ -1,8 +1,23 @@
 import hamburgerMenu from "./menu_hamburger.js";
+import clock from "./reloj_alarma.js";
+
 
 const d=document;
 d.addEventListener("DOMContentLoaded", e=>{           //Primero se carga el documento
-  hamburgerMenu("#panel-btn","#panel","#a");   //despues ahora si le paso los parametros
+  hamburgerMenu("#panel-btn","#panel","#a");   //despues ahora si mando llamar la funcion y le paso los parametros
+
+      //Ejercicio 1 Reloj
+  clock("#iniciarReloj","#detenerReloj","#mostrarReloj");
+
+
+
+
+
+
+
+
+
+
 });
 
 
--- Js/reloj_alarma.js
+++ Js/reloj_alarma.js
@@ -1,1 +1,34 @@
+const d=document;
 
+
+export default function clock(startClock, stopClock, showClock){
+  let clockTempo;
+  
+  d.addEventListener("click", e=>{
+
+    if(e.target.matches(startClock)){
+    
+      clockTempo = setInterval(()=>{
+        let time = new Date().toLocaleTimeString('en-US');      //ojo la variable debe estar dentro del interval, si no solo mostrara el segundo en que se ejecuto
+        console.log(time);
+
+        document.querySelector(showClock).innerHTML = `<h3>${time}<h3/>`        //selecciona el elemento que tiene el selector que me trajiste  
+      },1000);                                                                   // en 'showClock' y agregale un h3 y dentro la variable
+      
+      e.target.disabled = true;       // deshabilita el elemento que origino el evento en este caso fue el boton 'iniciarReloj'
+    }
+
+    if(e.target.matches(stopClock)){
+      console.log(e.target);
+
+      clearInterval(clockTempo);
+      d.querySelector(startClock).disabled = false;
+      d.querySelector(showClock).innerHTML = null;
+    }  
+  })
+}
+
+
+export function alarm(startAlarm, stopAlarm, alarm){
+
+}
