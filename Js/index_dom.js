dc3aa87c10642692cb1d751b045578dc6ae53223
Daniel Ayala <dayalaramz@outlook.com>
2022-03-28 20:56:39 -0500

    clock and alarm finish

--- Js/index_dom.js
+++ Js/index_dom.js
@@ -1,16 +1,16 @@
 import hamburgerMenu from "./menu_hamburger.js";
-import clock from "./reloj_alarma.js";
-
+import {clock,alarm} from "./reloj_alarma.js";
+//import alarm from "./reloj_alarma";
 
 const d=document;
 d.addEventListener("DOMContentLoaded", e=>{           //Primero se carga el documento
   hamburgerMenu("#panel-btn","#panel","#a");   //despues ahora si mando llamar la funcion y le paso los parametros
 
-      //Ejercicio 1 Reloj
+  //Ejercicio 1 Reloj
   clock("#iniciarReloj","#detenerReloj","#mostrarReloj");
+  alarm("#iniciarAlarma", "#detenerAlarma", "../assets/pianoManAlarm.mp3");
 
-
-
+  //Exercise 2
 
 
 
--- Js/reloj_alarma.js
+++ Js/reloj_alarma.js
@@ -1,7 +1,7 @@
 const d=document;
 
 
-export default function clock(startClock, stopClock, showClock){
+export function clock(startClock, stopClock, showClock){
   let clockTempo;
   
   d.addEventListener("click", e=>{
@@ -10,7 +10,7 @@ export default function clock(startClock, stopClock, showClock){
     
       clockTempo = setInterval(()=>{
         let time = new Date().toLocaleTimeString('en-US');      //ojo la variable debe estar dentro del interval, si no solo mostrara el segundo en que se ejecuto
-        console.log(time);
+        
 
         document.querySelector(showClock).innerHTML = `<h3>${time}<h3/>`        //selecciona el elemento que tiene el selector que me trajiste  
       },1000);                                                                   // en 'showClock' y agregale un h3 y dentro la variable
@@ -29,6 +29,29 @@ export default function clock(startClock, stopClock, showClock){
 }
 
 
-export function alarm(startAlarm, stopAlarm, alarm){
+export function alarm(startAlarm, stopAlarm, audio){
+let alarmTempo;
+const $alarm=d.createElement("audio");      //la variable se pone con $ al principio porque va guardar un elemento del dom
+      $alarm.src=audio;                   //a esa variable creada con el elemento audio entra a su ruta y  
+                                             //pegale el archivo de sonido que me estas pasando en el parametro audio
 
+      //<audio src="assets/pianoManAlarm.mp3" controls></audio>  esta etiqueta es la que crearemos pero no la mostraremos en el html
+
+  d.addEventListener("click", e=>{
+
+    if(e.target.matches(startAlarm)){
+      alarmTempo = setTimeout(() =>{
+        $alarm.play();          //despues de 1 segundo sonara el audio
+      },2000);
+      
+      e.target.disabled = true;     //deshabilita el boton presionado
+    }
+    
+    if(e.target.matches(stopAlarm)){
+      $alarm.pause();                                     //tambien es necesario este metodo, si no solo con el de currentTime no funciona bien
+      $alarm.currentTime = 0;                             //reinicia el audio que se esta reproduciendo
+      d.querySelector(startAlarm).disabled = false;       //habilita de nuevo el boton de start Alarm
+    }
+    
+  })
 }
